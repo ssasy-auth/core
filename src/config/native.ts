cc62c18bffa90935f38df792a17871dc58f19a61
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-03-07 13:11:39 +0100

    [config] adds lint-staged for linting before commit

--- src/config/native.ts
+++ src/config/native.ts
@@ -8,72 +8,61 @@ import { webcrypto } from "crypto";
 type Encoding = "utf8" | "base64";
 
 interface BufferEncoder {
-  /**
-   * Returns a buffer from a string
-   * 
-   * @param input - The string to encode
-   * @returns buffer
-   */
-  toBuffer: (input: string, encoding?: Encoding) => Uint8Array | ArrayBuffer;
+	/**
+	 * Returns a buffer from a string
+	 *
+	 * @param input - The string to encode
+	 * @returns buffer
+	 */
+	toBuffer: (input: string, encoding?: Encoding) => Uint8Array | ArrayBuffer;
 
-  /**
-   * Returns a string from a buffer
-   * 
-   * @param input - The buffer to decode
-   * @returns string
-   */
-  toString: (input: Uint8Array | ArrayBuffer, encoding?: Encoding) => string;
+	/**
+	 * Returns a string from a buffer
+	 *
+	 * @param input - The buffer to decode
+	 * @returns string
+	 */
+	toString: (input: Uint8Array | ArrayBuffer, encoding?: Encoding) => string;
 }
-
-/* Libraries to export */
-/**
- * The WebCrypto API
- */
-let WebCryptoLib: Crypto;
-/**
- * A Buffer encoder/decoder
- */
-let BufferLib: BufferEncoder;
 
 const isBrowser = typeof window !== "undefined";
 
-if (isBrowser) {
+/**
+ * The WebCrypto API
+ */
+export const WebCryptoLib: Crypto = isBrowser
+  ? window.crypto
+  : (webcrypto as unknown as Crypto);
 
-  if (!window.crypto) {
-    WebCryptoLib = window.crypto;
-  }
-
-  BufferLib = {
+/**
+ * A Buffer encoder/decoder
+ */
+export const BufferLib: BufferEncoder = isBrowser
+  ? ({
     toBuffer: (input, encoding) => {
-      return encoding === "base64" 
-        ? new Uint8Array(atob(input).split("")
-          .map(char => char.charCodeAt(0))) // atob(input) 
+      return encoding === "base64"
+        ? new Uint8Array(
+          atob(input)
+            .split("")
+            .map((char) => char.charCodeAt(0))
+        ) // atob(input)
         : new TextEncoder().encode(input);
     },
     toString: (input, encoding) => {
-      return encoding === "base64" 
-        ? btoa(input.toString()) 
+      return encoding === "base64"
+        ? btoa(input.toString())
         : new TextDecoder().decode(input);
     }
-  } as BufferEncoder;
-
-} else {
-
-  // Cast to `any` to avoid TypeScript errors since `webcrypto` is not available in Node.js by default
-  WebCryptoLib = webcrypto as unknown as Crypto;
-
-  BufferLib = {
+  } as BufferEncoder)
+  : ({
     toBuffer: (input, encoding) => {
-      return encoding === "base64" 
-        ? Buffer.from(input, "base64") 
-        : Buffer.from(input)
+      return encoding === "base64"
+        ? Buffer.from(input, "base64")
+        : Buffer.from(input);
     },
     toString: (input, encoding) => {
-      return encoding === "base64" 
-        ? Buffer.from(input).toString("base64") 
-        : Buffer.from(input).toString()
+      return encoding === "base64"
+        ? Buffer.from(input).toString("base64")
+        : Buffer.from(input).toString();
     }
-  } as BufferEncoder;
-}
-
-export { WebCryptoLib, BufferLib };
+  } as BufferEncoder);
